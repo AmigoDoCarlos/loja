7f183792f3a6d3ed3401e983bc87d146924102b3
AmigoDoCarlos <axelpinheiro@gmail.com>
2023-08-25 11:35:54 -0300

    tentando corrigir o carregamento das imagens

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/lojadoalex'
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/lojadoalex',
 }
 
 //module.exports = nextConfig
